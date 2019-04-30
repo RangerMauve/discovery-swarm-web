d15909bc56039848ecf88c87611bdeee35be17e5
RangerMauve <ranger@mauve.moe>
2019-04-30 16:46:28 -0400

    Version 1.0.7 - upgraded discovery-swarm-stream

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const DSS = require('discovery-swarm-stream/server')
 const websocket = require('websocket-stream')
-var defaults = require('dat-swarm-defaults')
-
-const SWARM_OPTS = defaults({
-  hash: false
-})
 
 module.exports = {
   createServer (opts) {
-    const dss = new DSS(SWARM_OPTS)
+    const dss = new DSS({})
 
     websocket.createServer(opts, (stream) => {
       dss.addClient(stream)
